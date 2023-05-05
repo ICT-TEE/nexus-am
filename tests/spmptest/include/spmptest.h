6c45a83686e488c317f7a80995c800fca8e29712
abcsml <a338383154@163.com>
2023-05-05 14:23:19 +0800

    spmp: add U,S mode (sum=1/0) test for spmp

--- tests/spmptest/include/spmptest.h
+++ tests/spmptest/include/spmptest.h
@@ -6,4 +6,28 @@
 #include <klib.h>
 #include <klib-macros.h>
 
+#include <csr.h>
+#include <xsextra.h>
+
+#define TEST_BASE  0x80010000
+#define INDEX_MASK 0x0000f000
+
+void init_spmp();
+void enable_spmp(uintptr_t pmp_reg, uintptr_t pmp_addr, uintptr_t pmp_size, uint8_t s, uint8_t permission);
+void enable_spmp_TOR(uintptr_t pmp_reg, uintptr_t pmp_addr, uintptr_t pmp_size, bool s, uint8_t permission);
+void disable_spmp(uintptr_t pmp_reg);
+
+void init_spmp_handler();
+
+void spmp_write_test(uint64_t addr);
+void spmp_read_test(uint64_t addr);
+void spmp_instr_test(uint64_t addr);
+
+void spmp_test_init_modeU();
+void spmp_test_init_modeS();
+
+void spmp_test_modeU();
+void spmp_test_modeS();
+void spmp_test_modeS_SUM();
+
 #endif
